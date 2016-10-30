53edf0074b5510129cbb556ec41d4b5ed5f36084
Evgenii Kuznetsov <Evgenii Kuznetsov>
2016-10-30 17:37:47 +0500

    Добавлены подсказки при наведении

--- src/js/main.js
+++ src/js/main.js
@@ -1,85 +1,34 @@
-(function($){
-	window.App = window.App || {};
-	App.Widgets = App.Widgets || {};
-	App.Objects = App.Objects || {};
-	App.Helpers = App.Helpers || {};
-	App.Locale = App.Locale || {};
-	App.User = App.User || {};
-
-	/**
-	 * Основной класс приложения
-	 * Все методы, начинающиеся с "init" запускаются автоматически при полной загрузке страницы
- 	 */
-	var Application = can.Control.extend(
-		{
-		},
-		{
-			init: function () {
-			},
-
-			bootstrap: function () {
-				var method;
-
-				for (method in this) {
-					if (method.length > 4 && method.substr(0, 4) === 'init') {
-						this[method]();
-					}
-				}
-
-				can.route.ready();
-			},
-
-			/**
-			 * Навешивает контроллер на DOM элемент и возвращает его instance
-			 * @param selector
-			 * @param controllerName
-			 * @param settings
-			 * @returns {*}
-			 */
-			installController: function (selector, controllerName, settings) {
-				settings = settings || {};
-				return this.element.find(selector)[controllerName](settings).control(controllerName);
-			},
-
-			/**
-			 * Инициадизация кастомных компонент вроде селектов, чекбоксов и прочего
-			 */
-			initCustomComponents: function () {
-				FastClick.attach(document.body);
+$(function () {
+	FastClick.attach(document.body);
 
 
-				var $root = $('.js-image'),
-					$elements = $root.find('.highlight');
+	var $root = $('.clothes-image'),
+		$highlight = $root.find('.clothes-image__highlight'),
+		$caption = $root.find('.clothes-image__caption');
 
-				$elements.on('click', function (e) {
-					$elements.each(function () {
-						this.classList.remove('hover');
-					});
+	/*$elements.on('click', function (e) {
+		$elements.each(function () {
+			var $t = $(this);
 
-					this.classList.add('hover');
-				});
+			$t.removeClass('hover');
+		});
 
-				$elements.on('mouseleave', function () {
-					$elements.each(function () {
-						this.classList.remove('hover');
-					});
-				});
+		this.classList.add('hover');
+	});*/
 
+	$highlight.on('mouseenter', function () {
+		var $t = $(this),
+			type = $t.data('type');
 
-			}
+		$highlight.removeClass('is-active');
+		$highlight.filter('[data-type=' + type + ']').addClass('is-active');
 
-			/*
-			initWidgets: function () {
-				this.installController('.js-header-menu', 'appWidgetsSearchHeader');
-				this.installController('.js-header-footer', 'appWidgetsMenu', {myOption: true});
-			}
-			*/
-		}
-	);
-
-	$(function(){
-		window.application = new Application('body');
-		window.application.bootstrap();
+		$caption.removeClass('is-active');
+		$caption.filter('[data-type=' + type + ']').addClass('is-active');
 	});
 
-}(jQuery));
+	$highlight.on('mouseleave', function () {
+		$highlight.removeClass('is-active');
+		$caption.removeClass('is-active');
+	});
+});
