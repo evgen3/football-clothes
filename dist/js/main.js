6947bd4ba1151f7e2d83247c26ef9d541922a0b4
Evgenii Kuznetsov <Evgenii Kuznetsov>
2016-10-30 18:02:57 +0500

    Добавлена кнопка переключения видимости подписей

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,7 +4,9 @@ $(function () {
 
 	var $root = $('.clothes-image'),
 		$highlight = $root.find('.clothes-image__highlight'),
-		$caption = $root.find('.clothes-image__caption');
+		$caption = $root.find('.clothes-image__caption'),
+		$button = $root.find('.clothes-image__button'),
+		visibleAll = false;
 
 	/*$elements.on('click', function (e) {
 		$elements.each(function () {
@@ -20,6 +22,9 @@ $(function () {
 		var $t = $(this),
 			type = $t.data('type');
 
+		if (visibleAll)
+			return;
+
 		$highlight.removeClass('is-active');
 		$highlight.filter('[data-type=' + type + ']').addClass('is-active');
 
@@ -28,7 +33,31 @@ $(function () {
 	});
 
 	$highlight.on('mouseleave', function () {
+		if (visibleAll)
+			return;
+		
 		$highlight.removeClass('is-active');
 		$caption.removeClass('is-active');
+	});
+
+	$button.on('click', function (e) {
+		var $t = $(this);
+		e.preventDefault();
+
+		if (visibleAll) {
+			$highlight.removeClass('is-active');
+			$caption.removeClass('is-active');
+			visibleAll = false;
+			$t.text($t.data('text'));
+			$t.removeClass('is-active');
+		} else {
+			$highlight.addClass('is-active');
+			$caption.addClass('is-active');
+			visibleAll = true;
+			$t.text($t.data('text-active'));
+			$t.addClass('is-active');
+		}
+
+
 	});
 });
